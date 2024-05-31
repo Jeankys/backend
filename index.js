8eeb541410232256ea91f84215a49d71844b8fc1
Jeankys <carlosriascos139@gmail.com>
2024-05-30 21:07:30 -0500

    v3

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 const express = require('express');
 const {urlencoded, json} = require('express');
+const router = require('./routes/rutas.js');
 const cors = require('cors');
-const router = require('./routes/tiquetes.routes.js');
+const mongoose = require('mongoose');
+require("dotenv").config();
+
 const app = express();
-const client= require ('./db/database.js');
-
-
-
 
 app.use(urlencoded({extended: true}))
 app.use(json())
+
 app.use(cors())
-app.use('/v1/tiquetes', router);
+app.use('/tiquetes', router);
 
-app.listen(4000, ()=>{
-    console.log('listening at port 4000');
+//mongodb connection
+mongoose.connect(process.env.MONGODB_URL)
+.then(() => console.log('Conexion de base de datos'))
+.catch((error) => console.error(error));
+
+
+app.listen(3000, ()=>{
+    console.log('listening at port 3000');
 })
-
